ed21c400a81ea828ed978018f070aa1322c6f547
Sean Murphy <sean@iamseanmurphy.com>
2024-11-29 20:18:10 +0000

    5.64.1

--- customer-reviews-woocommerce/includes/reminders/class-cr-local-forms.php
+++ customer-reviews-woocommerce/includes/reminders/class-cr-local-forms.php
@@ -311,30 +311,13 @@ if ( ! class_exists( 'CR_Local_Forms' ) ) :
 								`displayName` varchar(1024) DEFAULT NULL,
 								`formHeader` varchar(1024) DEFAULT NULL,
 								`formBody` varchar(1024) DEFAULT NULL,
-								`items` json DEFAULT NULL,
+								`items` text DEFAULT NULL,
 								`language` varchar(10) DEFAULT NULL,
 								`extra` text DEFAULT NULL,
 								PRIMARY KEY (`formId`),
 								KEY `orderId_index` (`orderId`)
 							) CHARACTER SET 'utf8mb4';" ) ) {
-						// it is possible that Maria DB is used that does not support JSON type
-						if( true !== $wpdb->query(
-								"CREATE TABLE `$table_name` (
-									`formId` varchar(190),
-									`orderId` varchar(190) DEFAULT NULL,
-									`customerEmail` varchar(1024) DEFAULT NULL,
-									`customerName` varchar(1024) DEFAULT NULL,
-									`displayName` varchar(1024) DEFAULT NULL,
-									`formHeader` varchar(1024) DEFAULT NULL,
-									`formBody` varchar(1024) DEFAULT NULL,
-									`items` text DEFAULT NULL,
-									`language` varchar(10) DEFAULT NULL,
-									`extra` text DEFAULT NULL,
-									PRIMARY KEY (`formId`),
-									KEY `orderId_index` (`orderId`)
-								) CHARACTER SET 'utf8mb4';" ) ) {
-							return array( 'code' => 1, 'text' => 'Table ' . $table_name . ' could not be created' );
-						}
+						return array( 'code' => 1, 'text' => 'Table ' . $table_name . ' could not be created' );
 					}
 				} else {
 					$table_name = $name_check;
